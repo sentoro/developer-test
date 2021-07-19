c53538aa03bf058a77ddc4328ab89ff9791d8b5d
Yevhen Humeniuk <mmg.gumeniuk.y@gmail.com>
2021-07-19 13:49:32 +0300

    added add new column, add new row and remove a row buttons

---
+++ resources/js/components/Notification.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-if="isError" class="alert notification" :class="`alert-${type}`" role="alert">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Notification',
+
+    props: {
+        isError: {
+            type: Boolean,
+            default: false,
+        },
+        type: {
+            type: String,
+            default: '',
+        },
+        message: {
+            type: String,
+            default: '',
+        }
+    },
+
+    data() {
+        return {}
+    },
+}
+</script>
+
+<style scoped>
+.notification {
+    position: absolute;
+    top: 20px;
+    right: 15px;
+}
+</style>
